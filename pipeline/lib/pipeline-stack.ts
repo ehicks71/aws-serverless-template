3aa1d77882042f335387ff858c6bec4987d31a30
Elizabeth Hicks <hicksem@wfu.edu>
2021-07-20 21:05:34 +0000

    Added buildspec.yml

---
+++ pipeline/lib/pipeline-stack.ts
@@ -0,0 +1,64 @@
+import * as cdk from '@aws-cdk/core';
+import s3 = require('@aws-cdk/aws-s3');
+import codepipeline = require('@aws-cdk/aws-codepipeline');
+import codepipeline_actions = require('@aws-cdk/aws-codepipeline-actions');
+import codebuild = require('@aws-cdk/aws-codebuild');
+
+export class PipelineStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    const artifactsBucket = new s3.Bucket(this, "ArtifactsBucket");
+    
+    // Pipeline creation starts
+    const pipeline = new codepipeline.Pipeline(this, 'Pipeline', {
+      artifactBucket: artifactsBucket
+    });
+    
+    // Declare source code as an artifact
+    const sourceOutput = new codepipeline.Artifact();
+
+    // Add source stage to pipeline
+    pipeline.addStage({
+      stageName: 'Source',
+      actions: [
+        new codepipeline_actions.GitHubSourceAction({
+          actionName: 'GitHub',
+          output: sourceOutput,
+          oauthToken: cdk.SecretValue.secretsManager('GITHUB_TOKEN_NAME2'),
+          trigger: codepipeline_actions.GitHubTrigger.POLL,
+          owner: 'hicksem',
+          repo: 'aws-workshop-sam-app2'
+        }),
+      ],
+    });
+    
+    // Declare build output as artifacts
+    const buildOutput = new codepipeline.Artifact();
+    
+    // Declare a new CodeBuild project
+    const buildProject = new codebuild.PipelineProject(this, 'Build', {
+      environment: { buildImage: codebuild.LinuxBuildImage.AMAZON_LINUX_2_2 },
+      environmentVariables: {
+        'PACKAGE_BUCKET': {
+          value: artifactsBucket.bucketName
+        }
+      }
+    });
+    
+    // Add the build stage to our pipeline
+    pipeline.addStage({
+      stageName: 'Build',
+      actions: [
+        new codepipeline_actions.CodeBuildAction({
+          actionName: 'Build',
+          project: buildProject,
+          input: sourceOutput,
+          outputs: [buildOutput],
+        }),
+      ],
+    });
+
+  }
+}
