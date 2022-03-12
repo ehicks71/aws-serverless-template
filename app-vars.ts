a0d63d8e55dd039f0797ca354982905adbd3c6ad
Elizabeth Hicks <hicksem@wfu.edu>
2022-03-11 22:44:59 -0500

    tweaks

--- app-vars.ts
+++ app-vars.ts
@@ -13,7 +13,7 @@
  * Do not add wfu-ait to the sAppBucketSlug. That is automatically done by the pipeline code.
  * Can be any length.
  */
-export const sAppBucketSlug = 'serverless-template';
+export const sAppBucketSlug = '[bucket slug]';
 
 /**
  * @var {string} sAppInitials
@@ -21,8 +21,8 @@ export const sAppBucketSlug = 'serverless-template';
  * Do not add WfuAit to the sAppInitials. That is automatically done by the pipeline code.
  * Keep the value to a max of3 letters.
  */
-export const sAppInitials = 'ST';
+export const sAppInitials = '[uppercase initial]';
 
-export const sRepositoryName = 'app-serverless-aws-template';
+export const sRepositoryName = '[repo template]';
 
-export const aStackEnvironments = ['Dev', 'Uat', 'Prod'];
+export const aStackEnvironments = [];
