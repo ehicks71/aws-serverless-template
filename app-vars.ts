a81a58c3b9f9c26f82976f7c796e9e0b94b75df4
Elizabeth Hicks <hicksem@wfu.edu>
2022-03-11 23:07:25 -0500

    Version 0.1.0 - Updates (#1)

---
+++ app-vars.ts
@@ -0,0 +1,28 @@
+/**
+ * If anything in this file is changed you need to do the following for the 
+ * changes to take effect.
+ * 
+ * cd pipeline
+ * npm run build
+ * cdk deploy [stack]
+ */
+ 
+/**
+ * @var {string} sAppBucketSlug
+ * 
+ * Do not add app prefix  to the sAppBucketSlug. That is automatically done by the pipeline code.
+ * Can be any length.
+ */
+export const sAppBucketSlug = '[bucket slug]';
+
+/**
+ * @var {string} sAppInitials
+ * 
+ * Do not add app prefix to the sAppInitials. That is automatically done by the pipeline code.
+ * Keep the value to a max of3 letters.
+ */
+export const sAppInitials = '[uppercase initial]';
+
+export const sRepositoryName = '[repo template]';
+
+export const aStackEnvironments = [];
